109a28174612cbfee6b57042f50144323f63aae7
ouvt02 <ouvt02@gmail.com>
2021-05-18 13:38:43 +0300

    Made through class inheritance

--- Program.cs
+++ Program.cs
@@ -9,93 +9,92 @@ using Microsoft.CodeAnalysis.CSharp;
 using System.Collections.Generic;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-int length_class_name = 3;
-int length_field_name = 2;
-int length_property_name = 5;
-int length_function_name = 1;
-int length_local_var_name = 3;
-
-String class_name = null;
-String property_name = null;
-String method_name = null;
-String field_name = null;
-String local_var_name = null;
 
 
-void GettingNames(SyntaxNode node)
+namespace classes
 {
-    if (node is VariableDeclaratorSyntax)
+    class Analyzer : CSharpSyntaxWalker
     {
-        if (node.Parent is VariableDeclarationSyntax)
+        int length_class_name = 3;
+        int length_field_name = 2;
+        int length_property_name = 5;
+        int length_function_name = 1;
+        int length_local_var_name = 3;
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
         {
-            if (node.Parent.Parent is FieldDeclarationSyntax)
+            String class_name = node.Identifier.ValueText;
+
+            if (class_name != null && class_name.Length > length_class_name)
+                Console.WriteLine($"Length of name of class \"{class_name}\" more than acceptable");
+
+            base.VisitClassDeclaration(node);
+        }
+
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            String method_name = node.Identifier.ValueText;
+
+            if (method_name != null && method_name.Length > length_function_name)
+                Console.WriteLine($"Length of name of function \"{method_name}\" more than acceptable");
+
+            base.VisitMethodDeclaration(node);
+        }
+
+        public override void VisitPropertyDeclaration(PropertyDeclarationSyntax node)
+        {
+            String property_name = node.Identifier.ValueText;
+
+                if (property_name != null && property_name.Length > length_property_name)
+        Console.WriteLine($"Length of name of property \"{property_name}\" more than acceptable");
+
+            base.VisitPropertyDeclaration(node);
+        }
+
+        public override void VisitVariableDeclarator(VariableDeclaratorSyntax node)
+        {
+            if (node.Parent is VariableDeclarationSyntax)
             {
-                VariableDeclaratorSyntax nameSyntax = (VariableDeclaratorSyntax)node;
-                field_name = nameSyntax.Identifier.Text;
+                if (node.Parent.Parent is FieldDeclarationSyntax)
+                {
+                    String field_name = node.Identifier.ValueText;
+
+                    if (field_name != null && field_name.Length > length_field_name)
+                        Console.WriteLine($"Length of name of field \"{field_name}\" more than acceptable");
+                }
+
+                else
+                {
+                    String local_var_name = node.Identifier.ValueText;
+
+                    if (local_var_name != null && local_var_name.Length > length_local_var_name)
+                        Console.WriteLine($"Length of name of local variable \"{local_var_name}\" more than acceptable");
+                }
             }
 
-            else
-            {
-                VariableDeclaratorSyntax nameSyntax = (VariableDeclaratorSyntax)node;
-                local_var_name = nameSyntax.Identifier.Text;
-            }
-
+            base.VisitVariableDeclarator(node);
         }
     }
 
-    else if (node is ClassDeclarationSyntax)
+    class Program
     {
-        ClassDeclarationSyntax nameSyntax = (ClassDeclarationSyntax)node;
-        class_name = nameSyntax.Identifier.Text;
-    }
+        static void Main(string[] args)
+        {
+            VisualStudioInstance[] test = MSBuildLocator.QueryVisualStudioInstances().ToArray();
+            VisualStudioInstance inst = test[0];
+            MSBuildLocator.RegisterInstance(inst);
 
-    else if (node is PropertyDeclarationSyntax)
-    {
-        PropertyDeclarationSyntax nameSyntax = (PropertyDeclarationSyntax)node;
-        property_name = nameSyntax.Identifier.Text;
-    }
+            MSBuildWorkspace workspace = MSBuildWorkspace.Create();
+            Project project = workspace.OpenProjectAsync(@"C:\Users\Ульяна\source\repos\ForTesting\ForTesting.csproj").Result;
+            IEnumerable<Document> documents = project.Documents;
 
-    else if (node is MethodDeclarationSyntax)
-    {
-        MethodDeclarationSyntax nameSyntax = (MethodDeclarationSyntax)node;
-        method_name = nameSyntax.Identifier.Text;
+            Document[] Documents = documents.ToArray();
+
+            Analyzer analyzer = new Analyzer();
+
+            foreach (Document document in documents)
+                analyzer.Visit(document.GetSyntaxTreeAsync().Result.GetRoot());
+        }
     }
 }
 
-void Visit(SyntaxNode node)
-{
-    GettingNames(node);
-
-    if (class_name != null && class_name.Length > length_class_name)
-        Console.WriteLine($"Length of name of class \"{class_name}\" more than acceptable");
-
-    if (property_name != null && property_name.Length > length_property_name)
-        Console.WriteLine($"Length of name of property \"{property_name}\" more than acceptable");
-
-    if (method_name != null && method_name.Length > length_function_name)
-        Console.WriteLine($"Length of name of function \"{method_name}\" more than acceptable");
-
-    if (field_name != null && field_name.Length > length_field_name)
-        Console.WriteLine($"Length of name of field \"{field_name}\" more than acceptable");
-
-    if (local_var_name != null && local_var_name.Length > length_local_var_name)
-        Console.WriteLine($"Length of name of local variable \"{local_var_name}\" more than acceptable");
-
-    IEnumerable < SyntaxNode > nodes = node.ChildNodes();
-
-    foreach (SyntaxNode child_node in nodes)
-        Visit(child_node);
-}
-
-VisualStudioInstance[] test = MSBuildLocator.QueryVisualStudioInstances().ToArray();
-VisualStudioInstance inst = test[0];
-MSBuildLocator.RegisterInstance(inst);
-
-MSBuildWorkspace workspace = MSBuildWorkspace.Create();
-Project project = workspace.OpenProjectAsync(@"C:\Users\Ульяна\source\repos\ForTesting\ForTesting.csproj").Result;
-IEnumerable<Document> documents = project.Documents;
-
-Document[] Documents = documents.ToArray();
-
-foreach (Document document in documents)
-    Visit(document.GetSyntaxTreeAsync().Result.GetRoot());
